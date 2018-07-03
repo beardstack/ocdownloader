344b519963d74500acf7cf34a8a752cbef579d83
microwavecausesaids <33128510+microwavecausesaids@users.noreply.github.com>
2018-07-03 04:07:12 -0400

    Update youtube.php

--- controller/lib/youtube.php
+++ controller/lib/youtube.php
@@ -217,7 +217,7 @@ class YouTube
     }
 
 
-    public function getVideoData($ExtractAudio = false)
+    public function download($ExtractAudio = false)
     {
         $Proxy = null;
         if (!is_null($this->ProxyAddress) && $this->ProxyPort > 0 && $this->ProxyPort <= 65536) {
@@ -240,41 +240,7 @@ class YouTube
 	
 	$this->syncDownloadsFolder();
 	error_log("HERE --> Scan done", 0);
-/*
-     $index=(preg_match('/&index=(\d+)/', $this->URL, $current))?$current[1]:1;
 
-         if (!is_null($Output)) {
-            $Output = explode("\n", $Output);
-
-            if (count($Output) >= 2) {
-                $OutProcessed = array();
-                $current_index=1;
-                 for ($I = 0; $I < count($Output); $I++) {
-                    if (mb_strlen(trim($Output[$I])) > 0) {
-                        if (mb_strpos(urldecode($Output[$I]), 'https://') === 0
-                                && mb_strpos(urldecode($Output[$I]), '&mime=video/') !== false) {
-                            $OutProcessed['VIDEO'] = $Output[$I];
-                        } elseif (mb_strpos(urldecode($Output[$I]), 'https://') === 0
-                                && mb_strpos(urldecode($Output[$I]), '&mime=audio/') !== false) {
-                            $OutProcessed['AUDIO'] = $Output[$I];
-                        } else {
-                            $OutProcessed['FULLNAME'] = $Output[$I];
-                        }
-                    }
-                 if ((!empty($OutProcessed['VIDEO']) || !empty($OutProcessed['AUDIO'])) && !empty($OutProcessed['FULLNAME']))
-                    {
-                        if ($index == $current_index)
-                        {
-                           break;
-                        } else {
-                            $OutProcessed = array();
-                            $current_index++;
-                        }
-                    }
-                } 
-                return $OutProcessed;
-            }
-       } */
         return null;
     } 
 
