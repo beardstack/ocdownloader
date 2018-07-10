df24f8f4519783d1c62481ff114d5610269137b1
microwavecausesaids <33128510+microwavecausesaids@users.noreply.github.com>
2018-07-09 23:50:56 -0400

    Update sql.php

--- controller/lib/sql.php
+++ controller/lib/sql.php
@@ -5,22 +5,46 @@ namespace OCA\ocDownloader\Controller\Lib;
 
 class db_queue
 {
+        /*
+        "ALL" = 5
+        "COMPLETES" = 0
+        "ACTIVE" = 1
+        "WAITINGS" = 2
+        "STOPPED" = 3
+        "REMOVED" = 4
+         */      
+        public function add($a) {
+                $SQL = 'INSERT INTO `*PREFIX*ocdownloader_queue`
+                (`UID`, `GID`, `FILENAME`, `PROTOCOL`, `STATUS`, `TIMESTAMP`) VALUES(?, ?, ?, ?, ?, ?)';
+                
+                if ($this->DbType == 1) {
+                    $SQL = 'INSERT INTO *PREFIX*ocdownloader_queue
+                        ("UID", "GID", "FILENAME", "PROTOCOL", "STATUS", "TIMESTAMP") VALUES(?, ?, ?, ?, ?, ?)';
+                }
+                $Query = \OCP\DB::prepare($SQL);
+                $Result = $Query->execute(array(
+                    $a['UID'],
+                    $a['GID'],
+                    $a['FILENAME'],
+                    $a['PROTOCOL'],
+                    1, # ACTIVE
+                    time()
+                ));
+        }
 
-        $SQL = 'INSERT INTO `*PREFIX*ocdownloader_queue`
-            (`UID`, `GID`, `FILENAME`, `PROTOCOL`, `STATUS`, `TIMESTAMP`) VALUES(?, ?, ?, ?, ?, ?)';
-        #if ($this->DbType == 1) {
-        #    $SQL = 'INSERT INTO *PREFIX*ocdownloader_queue
-        #        ("UID", "GID", "FILENAME", "PROTOCOL", "STATUS", "TIMESTAMP") VALUES(?, ?, ?, ?, ?, ?)';
-        #}
-        #$Query = \OCP\DB::prepare($SQL);
-        #$Result = $Query->execute(array(
-        #    $this->CurrentUID,
-        #    $AddURI['result'],
-        #    $Target,
-        #    strtoupper(substr($_POST['FILE'], 0, strpos($_POST['FILE'], ':'))),
-        #    1,
-        #    time()
-        #));
-
-
+        public function update($a)
+            {
+                $SQL = 'UPDATE `*PREFIX*ocdownloader_queue`
+                            SET `STATUS` = ? WHERE `UID` = ? AND `GID` = ?';
+                        if ($this->DbType == 1) {
+                            $SQL = 'UPDATE *PREFIX*ocdownloader_queue
+                                SET "STATUS" = ? WHERE "UID" = ? AND "GID" = ?';
+                        }
+                        $Query = \OCP\DB::prepare($SQL);
+                        $Result = $Query->execute(array(
+                                $a['STATUS'], 
+                                $a['UID'],
+                                $a['GID']
+                        ));
+            }
 }
