24d8327123058a73b7d84481fec32fdd5260edb8
microwavecausesaids <33128510+microwavecausesaids@users.noreply.github.com>
2018-07-10 22:11:55 -0400

    Update exec_yt.php

--- controller/lib/exec_yt.php
+++ controller/lib/exec_yt.php
@@ -4,5 +4,24 @@ namespace OCA\ocDownloader\Controller;
 
 use OCA\ocDownloader\Controller\Lib\YouTube;
 
-$YouTube = new YouTube($this->YTDLBinary, $_POST['FILE']);
+/*
 
+
+$argv[0] = youtube-dl path
+$argv[1] = UID
+$argv[2] = DID
+
+Need to gather youtube-dl path
+Need to gather UID (User ID)
+Need to gather DID Param (UniqueID for the directory in which configs are stored)
+
+Downloading specs are in the /tmp/ytdownloader/$ID directory
+
+
+*/
+
+error_log("HERE --> Parsed to execute" . $argv[0]. " ". $argv[1] . " " . $argv[2]  , 0);
+
+#$YouTube = new YouTube($this->YTDLBinary, $_POST['FILE']);
+
+?>
