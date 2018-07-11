063b12257a9b6c9d661d8a8a16992eed75fea2d7
microwavecausesaids <33128510+microwavecausesaids@users.noreply.github.com>
2018-07-11 00:54:45 -0400

    Update exec_yt.php

--- controller/lib/exec_yt.php
+++ controller/lib/exec_yt.php
@@ -22,6 +22,8 @@ Downloading specs are in the /tmp/ytdownloader/$ID directory
 
 error_log("ARGUMENTS!!!! ----> " . $argv[1]. " ". $argv[2] . " " . $argv[3]  , 0);
 $YouTube = new YouTube($argv[1], $argv[2], $argv[3] );
+error_log("2" , 0);
+
 $YouTube->dl();
 error_log("OOM" , 0);
 
